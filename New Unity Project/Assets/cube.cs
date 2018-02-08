2b6e5e02edb64e66733d687389433ec6692f388a
Cezar Savin <cezarsavin13@gmail.com>
2018-02-08 23:55:31 +0100

    Initial commit
    Initial commit

---
+++ New Unity Project/Assets/cube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+		var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
